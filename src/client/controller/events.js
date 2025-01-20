801f58fc3ad0fb6caed0a222abb25261382f0be6
Héctor Adán <hectorino2789@gmail.com>
2025-01-19 22:52:36 -0600

    Fixing some breadcrump-menu issues

--- src/client/controller/events.js
+++ src/client/controller/events.js
@@ -3,54 +3,37 @@
 // Importar las clases necesarias
 import { DataExplorer } from "./dataExplorer.js"; // Asegúrate de que el archivo de la clase esté en la misma carpeta o ajusta la ruta
 
-// Crear las carpetas principales usando DataExplorer
-const rootFolders = {
-    "All Documents": new DataExplorer([
-        ["29384665555555555", "/", "Desktop/",
-            [      
-                ["29384r857857t5589", "file", "main.js", "19/11/24", "noshared", "664"],
-                ["29384r857857t5589", "file", "functions.js", "19/11/24", "noshared", "654"],
-                ["29384r857857t5589", "file", "events.js", "19/11/24", "noshared", "400"],
+let breadcrumbState = ["All Documents"];
+const breadcrumbContainer = document.querySelector(".breadcrump");
+const tableContent = document.querySelector(".content");
+let temporalElements; 
 
-                ["845586u867877788", "/", "model/", 
-                    ["59866666667777784", "file", "query.js", "19/11/24", "noshared", "664"],
-                    ["87444455476565473", "file", "fetchData.js", "19/11/24", "noshared", "654"],
-                    ["93745756757675676", "file", "dataHandler.js", "19/11/24", "noshared", "400"]
-                ]
-            ]
-        ],
-        ["29384665555555555", "/", "Pictures/",
-            [      
-                ["29384r857857t5589", "file", "baskground.png", "19/11/24", "noshared", "664"],
-                ["29384r857857t5589", "file", "portrait.jpg", "19/11/24", "noshared", "654"],
-                ["29384r857857t5589", "file", "birthday.mp4", "19/11/24", "noshared", "400"],
-
-                ["845586u867877788", "/", "Wallpapers/", 
-                    ["59866666667777784", "file", "dynamicIsland.png", "19/11/24", "noshared", "664"],
-                    ["87444455476565473", "file", "user.ico", "19/11/24", "noshared", "654"],
-                    ["93745756757675676", "file", "hackerProfile.png", "19/11/24", "noshared", "400"]
-                ]
-            ]
-        ]
-    ]),
-    "Files": new DataExplorer([]),
-    "Folders": new DataExplorer([]),
-    "Recent": new DataExplorer([]),
-    "Favorite Files": new DataExplorer([]),
-};
+let temporal = {
+    "All Documents": 
+    [
+        ["Pictures/", "/"],
+        ["header.h", "file", "45 M", "444", "23/Nov/24", "noshared"]
+    ],
+    "Files": [],
+    "Folders": [],
+    "Recent": [],
+    "Favorite Files": [],
+    "Pictures": [
+        ["Node Filer/", "/"],
+    ],
+    "Node Filer": [
+        ["index.html", "file", "3 Kb", "644", "6/Jan/25", "noshared"],
+    ]
+}
 
 // Función para actualizar la tabla en el DOM
 const updateTable = (item, elementNode) => {
-    let contentFolder = rootFolders[item].data; // Obtener los datos de la carpeta correspondiente
-
-    if (item === "All Documents") {
-        contentFolder = rootFolders[item].getAllData();
-        rootFolders[item].removeTrash();
-    }
+    
+    // temporalElements ? temporalElements.forEach(element => element.removeEventListener("dblclick", () => {})): "" ;
 
     elementNode.innerHTML = ""; // Limpiar el contenido actual
     
-    if (!contentFolder.length) {
+    if (temporal[item].length == 0) {
         elementNode.innerHTML = `
             <div class="advice__content">
                 <img src="resources/Logos/FileNotFound.webp" width="200px">
@@ -59,6 +42,8 @@ const updateTable = (item, elementNode) => {
             </div>`;
         return;
     }
+
+    console.log(temporal[item])
 
     let tableContent = `
         <table class="table__content">
@@ -71,60 +56,93 @@ const updateTable = (item, elementNode) => {
             </tr>`;
     
     // Recorriendo los contenidos usando el método de DataExplorer
-    contentFolder.forEach(element => {
-        console.log(element);
-        if (Array.isArray(element)) {
+    temporal[item].forEach(element => {
             // Si es un directorio o archivo, lo mostramos
-            if (element[1] === "/") {
-                tableContent += `
-                    <tr class="table__head">
-                        <td class="name__head">${element[2]}</td>
-                        <td class="document__weight"></td>
-                        <td class="document__permission"></td>
-                        <td class="document__info"></td>
-                        <td class="document__status"></td>
-                    </tr>`;
-            }
-            else if (element[1] === "file") {
-                tableContent += `
-                    <tr class="table__head">
-                        <td class="name__head">${element[2]}</td>
-                        <td class="document__weight">${(element[4]) ? element[4] + " KB" : " "}</td>
-                        <td class="document__permission">${element[5] || " "}</td>
-                        <td class="document__info">${element[3]}</td>
-                        <td class="document__status">${element[5]}</td>
-                    </tr>`;
-            }
+        if (element[1] === "/") {
+            tableContent += `
+                <tr class="table__head menu__items folder">
+                    <td class="name__head">${element[0]}</td>
+                    <td class="document__weight"></td>
+                    <td class="document__permission"></td>
+                    <td class="document__info"></td>
+                    <td class="document__status"></td>
+                </tr>`;
         }
-    });
+        else if (element[1] === "file") {
+            tableContent += `
+                <tr class="table__head menu__items file">
+                    <td class="name__head">${element[0]}</td>
+                    <td class="document__weight">${(element[2]) ? element[2] : " "}</td>
+                    <td class="document__permission">${element[3] || " "}</td>
+                    <td class="document__info">${element[4]}</td>
+                    <td class="document__status">${element[5]}</td>
+                </tr>`;
+        }
+    })
 
     tableContent += "</table>";
     elementNode.innerHTML = tableContent;
+
+
+    temporalElements = document.querySelectorAll(".folder");
+    for (let items of temporalElements) {
+        items.addEventListener("dblclick", () => {
+            updateBreadcrumbs(items.childNodes[1].textContent.slice(0, -1))
+            updateTable(items.childNodes[1].textContent.slice(0, -1), document.querySelector(".content"));
+
+        })
+    }
+
 };
 
-// Función para actualizar el breadcrumbs en el DOM
-const updateBreadcrumbs = (breadcrumbContainer, item, containerTable) => {
-    breadcrumbContainer.innerHTML = ""; // Limpiar el contenedor
+/**
+* Update de BreadCrump menu based on the user 
+* directory position.
+*
+* @param {itemName} string      Indicates the directory's name  
+*
+**/
+const updateBreadcrumbs = (itemElement) => {
+    /* It's simple, just add the actual Folder's name to the breadCrump array
+    */
+    itemElement ? breadcrumbState.push(itemElement): undefined;
+
+    // Removes all the breadcrump menu's content
+    breadcrumbContainer.innerHTML = ""; 
+
+
     const fragment = document.createDocumentFragment();
-    const breadcrumbItem = document.createElement("p");
-    const breadcrumbseparator = document.createElement("p");
+    let breadcrumbItem, breadcrumbSeparator;
 
-    breadcrumbItem.textContent = item;
-    breadcrumbseparator.textContent = ">";
+    
+    for (let elementNode of breadcrumbState) {
+        breadcrumbItem = document.createElement("p");
+        breadcrumbSeparator = document.createElement("p");
 
-    fragment.appendChild(breadcrumbItem);
-    fragment.appendChild(breadcrumbseparator)
+        breadcrumbItem.textContent = elementNode;
+        breadcrumbSeparator.textContent = ">";
+        
+        breadcrumbItem.addEventListener("click", (item) => {
+
+            breadcrumbState = breadcrumbState.slice(0, 
+                breadcrumbState.indexOf(item.target.textContent) + 1
+            );
+            updateBreadcrumbs();
+            updateTable(item.target.textContent, tableContent);
+        })
+
+        fragment.appendChild(breadcrumbItem);
+        fragment.appendChild(breadcrumbSeparator)
+    }
 
     breadcrumbContainer.appendChild(fragment);
 
-    updateTable(item, containerTable); // Pasar el nombre de la carpeta al actualizar la tabla
 };
+
 
 // Inicialización de eventos en el DOM
 document.addEventListener("DOMContentLoaded", () => {
     const optionsItems = document.querySelectorAll(".option__item");
-    const breadcrumbContainer = document.querySelector(".breadcrump");
-    const tableContent = document.querySelector(".content");
 
     // Añadir eventos a las opciones
     optionsItems.forEach((option) => {
@@ -137,10 +155,13 @@ document.addEventListener("DOMContentLoaded", () => {
             element.target.classList.add("element__selected");
 
             // Obtener el nombre del directorio de textContent
-            const itemName = element.target.textContent;
+            const itemName = element.target;
 
             // Actualizar breadcrumbs y la tabla
-            updateBreadcrumbs(breadcrumbContainer, itemName, tableContent);
+            // breadcrumbState.pop()
+            breadcrumbState.length = 0;
+            updateBreadcrumbs(itemName.textContent);
+            updateTable(itemName.textContent, tableContent);
         });
     });
 
